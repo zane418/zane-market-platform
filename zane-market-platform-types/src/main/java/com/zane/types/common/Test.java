3fb884bf4eb289b663e791f44c5328bf2b1880d3
zane <caizhenlong07@163.com>
2024-04-29 18:25:10 +0800

    feat: database-design

--- zane-market-platform-types/src/main/java/com/zane/types/common/Test.java
+++ zane-market-platform-types/src/main/java/com/zane/types/common/Test.java
@@ -7,5 +7,6 @@ package com.zane.types.common;
 public class Test  {
     public static void main(String[] args) {
         System.out.println("123132");
+        System.out.println(1231312);
     }
 }
